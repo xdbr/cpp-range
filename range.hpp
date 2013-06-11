23836302a7fa55acd6414e101f8ed356ebf802b7
Daniel Bruder <daniel.bruder@gmail.com>
2013-06-11 21:39:34 +0200

    add Rakefile

---
+++ range.hpp
@@ -0,0 +1,29 @@
+/*
+
+    range.cpp
+
+    range generator
+*/
+
+#include <list>
+
+// TODO: better yet, return generators (function pointers)...
+
+template <class T>
+class range {
+    T               begin_;
+    T               end_;
+    T               step_;
+    std::list<T>    list_;
+public:
+    range(T begin, T end, T step = 1)
+        : begin_(begin), end_(end), step_(step)
+        {
+            for (T i = begin_; i <= end_; i += step_) {
+                list_.push_back(i);
+            }
+        }
+    inline auto begin() -> decltype(list_.begin()) { return list_.begin(); }
+    inline auto end()   -> decltype(list_.end())   { return list_.end();   }
+    inline auto get()   -> decltype(list_)         { return list_; }
+};
---
+++ test_range.cpp
@@ -0,0 +1,53 @@
+/*
+
+    test_range.cpp
+
+    test range generator
+*/
+
+#include "range.hpp"
+
+#include <iostream>
+#include <cassert>
+#include <list>
+
+/* Compile with -D_TEST_DEBUG to see output */
+// #define _TEST_DEBUG
+
+int main() {
+
+    std::list<int> list_int = { 0, 2, 4, 6, 8, 10 };
+
+    #ifdef _TEST_DEBUG
+    std::cout << "Test range<int>(0, 10, 2)" << std::endl;
+    for (const auto & i : range<int>(0, 10, 2))
+        std::cout << i << std::endl;
+    #endif /* _TEST_DEBUG */
+    
+    assert( list_int == range<int>(0, 10, 2).get() );
+
+    // harr, harr.
+    std::list<char> list_char = { 'a', 'd', 'g', 'j', 'm', 'p', 's', 'v', 'y' };
+
+    #ifdef _TEST_DEBUG
+    std::cout << "Test range<char>('a', 'z', 3)" << std::endl;
+    for (const auto & l :range<char>('a', 'z', 3))
+        std::cout << l << std::endl;
+    #endif /* _TEST_DEBUG */
+    
+    assert( list_char == range<char>('a', 'z', 3).get() );
+
+
+
+    std::list<long> list_long = { -2, -1, 0, 1, 2 };
+    
+    #ifdef _TEST_DEBUG
+    std::cout << "Test range<int>(-2, 2)" << std::endl;
+    for (const auto & l : range<long>(-2, 2))
+        std::cout << l << std::endl;
+    #endif /* _TEST_DEBUG */
+    assert( list_long == range<long>(-2, 2).get() );
+
+    std::cout << "All tests successful." << std::endl;
+    return 0;
+}
